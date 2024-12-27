b425a0960c82106c0a3074203fe8c7abd05fc61e
Anton Serdyuchenko <anton415@gmail.com>
2024-12-27 18:53:57 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Anton Serdyuchenko on 27.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
