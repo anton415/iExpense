eb353146fd552ecfdbe7a3ecaf3c1bac2d0468d8
Anton Serdyuchenko <anton415@gmail.com>
2025-01-06 17:06:12 +0300

    Styling item's amount.

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -52,7 +52,9 @@ struct ContentView: View {
                         }
 
                         Spacer()
-                        Text(item.amount, format: .currency(code: "USD"))
+                        Text(item.amount, format: .currency(code: Locale.current.currency!.identifier))
+                            .foregroundStyle(item.amount < 100  ? .blue : .red)
+                            .foregroundStyle(item.amount < 10 ? .black : .blue)
                     }
                 }
                 .onDelete(perform: removeItems)
